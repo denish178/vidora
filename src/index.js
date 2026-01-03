ff1021f6af30cb67c93360a2acbaf7df9754780e
Denish Murawala <murawaladenish@gmail.com>
2026-01-04 01:26:29 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./contants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !! ", err);
+  });
 
 // import express from "express";
 // const app = express();
